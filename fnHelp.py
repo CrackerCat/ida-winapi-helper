6af41c40553f43b84a882827908cf7ee6c8cf5e3
Suvaditya <x0r19x91@users.noreply.github.com>
2021-03-01 10:52:23 +0530

    Update fnHelp.py

--- fnHelp.py
+++ fnHelp.py
@@ -1,6 +1,6 @@
 import requests, idaapi, os, idc, json
 
-TARGET = 'D:\\msdn.json'
+TARGET = 'D:\\winapi.json'
 funcDB = {}
 
 def InitFn():
@@ -10,25 +10,28 @@ def InitFn():
         funcDB = json.load(open(TARGET))
         return
     print "Creating DB ..."
-    headers = requests.get('https://docs.microsoft.com/en-us/windows/win32/api/toc.json').json()
-    for hdr in headers['items'][0]['children'][1]['children']:
-        title = hdr['toc_title'].decode()
-        print title
-        data = requests.get('https://docs.microsoft.com/en-us/windows/win32/api/%s/toc.json' % hdr['href'].decode()).json()
-        if data['items'] and data['items'][0] and 'children' in data['items'][0].keys():
-            for child in data['items'][0]['children']:
-                try:
-                    tag = child['toc_title'].decode()
-                    if 'function' in tag:
-                        href = child['href'].decode()
-                        fn = tag.split()[0]
-                        # print "Func: ", fn
-                        funcDB[fn] = 'https://docs.microsoft.com/en-us/' + href
-                except Exception as e:
-                    import traceback
-                    print(child)
-                    traceback.print_exc()
-                    return
+    def cache(prefix):
+        headers = requests.get('%stoc.json' % prefix).json()
+        for hdr in headers['items'][0]['children'][1]['children']:
+            title = hdr['toc_title'].decode()
+            # print title
+            data = requests.get('%s%s/toc.json' % (prefix, hdr['href'].decode())).json()
+            if data['items'] and data['items'][0] and 'children' in data['items'][0].keys():
+                for child in data['items'][0]['children']:
+                    try:
+                        tag = child['toc_title'].decode()
+                        if 'function' in tag:
+                            href = child['href'].decode()
+                            fn = tag.split()[0]
+                            # print "Func: ", fn
+                            funcDB[fn] = 'https://docs.microsoft.com/en-us/' + href
+                    except Exception as e:
+                        import traceback
+                        print(child)
+                        traceback.print_exc()
+                        return
+    cache("https://docs.microsoft.com/en-us/windows/win32/api/")
+    cache("https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/")
     with open(TARGET, "w") as fp:
         fp.write(json.dumps(funcDB))
     print("Done!")
